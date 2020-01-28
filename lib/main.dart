7b806389a238bc6bb06cbd4cb6eeddb1ed5b1455
mouhcine mimya <mouhcine.mimya@gmail.com>
2020-01-28 23:59:54 +0100

    check if device if connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:connectivity/connectivity.dart';
 import 'package:avatar_glow/avatar_glow.dart';
+import 'package:wifi/wifi.dart';
+import 'package:ping_discover_network/ping_discover_network.dart';
 
 void main() => runApp(MyApp());
 
@@ -25,15 +29,50 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  bool _lampStatus = true;
+  String _lampStatus = "off";
   bool _connectivityWifi = false;
+  String _deviceAdress = null;
   var subscription;
-  /* void ConnectivityWifi() async {
-    var connectivityResult = await (Connectivity().checkConnectivity());
-    if (connectivityResult == ConnectivityResult.wifi) {
-      // I am connected to a wifi network.
+
+  void connectedWifiDevices(int port) async {
+    final String ip = await Wifi.ip;
+    final String subnet = ip.substring(0, ip.lastIndexOf('.'));
+    final stream = NetworkAnalyzer.discover2(subnet, port);
+    int found = 0;
+    stream.listen((NetworkAddress addr) {
+      if (addr.exists) {
+        found++;
+        print('Found device: ${addr.ip}:$port');
+        setState(() {
+          _deviceAdress = '${addr.ip}:$port';
+        });
+      }
+    }).onDone(() async {
+      print('Finish. Found $found device(s)');
+      if (found == 0) {
+        setState(() {
+          _deviceAdress = null;
+        });
+      } else {
+        initLampStatus();
+      }
+    });
+  }
+
+  void initLampStatus() async {
+    var response = await http.get("http://$_deviceAdress/status");
+    print(response.body);
+    if (response.statusCode == 200) {
+      String resStatus = json.decode(response.body)['26'];
+      print("resStatus " + resStatus);
+      setState(() {
+        _lampStatus = resStatus;
+      });
+    } else {
+      print("error");
     }
-  } */
+  }
+
   @override
   initState() {
     super.initState();
@@ -45,11 +84,12 @@ class _HomePageState extends State<HomePage> {
       print(" switched ");
       if (result == ConnectivityResult.wifi) {
         print("Connected to wifi ");
+        connectedWifiDevices(89);
         setState(() {
           _connectivityWifi = true;
         });
       } else {
-         setState(() {
+        setState(() {
           _connectivityWifi = false;
         });
       }
@@ -64,51 +104,62 @@ class _HomePageState extends State<HomePage> {
   }
 
   Future<void> _action() async {
-    print("presed");
-    setState(() {
-      _lampStatus = !_lampStatus;
-    });
-    /*  var response = await http.get('http://46.101.148.188/api/status');
-    print(response.body);
-    if (response.statusCode == 200) {
-      print("success");
-      setState(() {
-        _lampStatus = !_lampStatus;
-      });
+    if (_deviceAdress != null) {
+      String statusToSend = _lampStatus == "on" ? "off" : "on";
+      print(statusToSend);
+      var response = await http.get("http://$_deviceAdress/26/$statusToSend");
+      print(response.body);
+      if (response.statusCode == 200) {
+        String resStatus = json.decode(response.body)['status'];
+        print(resStatus == "on");
+        setState(() {
+          _lampStatus = resStatus;
+        });
+      } else {
+        print("error");
+      }
     } else {
-      print("error");
-    } */
+      print('scan for device');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-      child:_connectivityWifi?
-      AvatarGlow(
-        startDelay: Duration(milliseconds: 1000),
-        glowColor: _lampStatus ? Colors.yellow : Colors.black,
-        endRadius: 100.0,
-        duration: Duration(milliseconds: 2000),
-        repeat: true,
-        showTwoGlows: true,
-        repeatPauseDuration: Duration(milliseconds: 100),
-        child: Material(
-          elevation: 8.0,
-          color: Theme.of(context).primaryColor,
-          shape: CircleBorder(),
-          child: Container(
-            child: IconButton(
-              color: _lampStatus ? Colors.yellow : Colors.grey,
-              icon: Icon(Icons.lightbulb_outline),
-              onPressed: () => _action(),
-            ),
-          ),
-        ),
-        shape: BoxShape.circle,
-        animate: true,
-        curve: Curves.fastOutSlowIn,
-      ):Text("Please Connect to wifi network")
-    ));
+            child: _connectivityWifi
+                ? _deviceAdress != null
+                    ? AvatarGlow(
+                        startDelay: Duration(milliseconds: 1000),
+                        glowColor:
+                            _lampStatus == "on" ? Colors.yellow : Colors.black,
+                        endRadius: 100.0,
+                        duration: Duration(milliseconds: 2000),
+                        repeat: true,
+                        showTwoGlows: true,
+                        repeatPauseDuration: Duration(milliseconds: 100),
+                        child: Material(
+                          elevation: 8.0,
+                          color: Theme.of(context).primaryColor,
+                          shape: CircleBorder(),
+                          child: Container(
+                            child: IconButton(
+                              color: _lampStatus == "on"
+                                  ? Colors.yellow
+                                  : Colors.grey,
+                              icon: Icon(Icons.lightbulb_outline),
+                              onPressed: () => _action(),
+                            ),
+                          ),
+                        ),
+                        shape: BoxShape.circle,
+                        animate: true,
+                        curve: Curves.fastOutSlowIn,
+                      )
+                    : IconButton(
+                        icon: Icon(Icons.refresh),
+                        onPressed: () => connectedWifiDevices(89),
+                      )
+                : Text("Please Connect to wifi network")));
   }
 }
